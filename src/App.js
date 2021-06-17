a43c9c07c0dffc40e0f3b7fe51c97fc7fc7b66d2
Ayush raj anand <rajanand.ayush98@gmail.com>
2021-06-17 23:34:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { ChatEngine } from 'react-chat-engine'
+import Chatfeed from './components/Chatfeed';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ChatEngine
+        height="100vh"
+        projectID="57588c20-e26d-4c5f-b650-df6c937cd628"
+        userName="Ayush"
+        userSecret="123123"
+        renderChatFeed={(chatAppState) => <Chatfeed {...chatAppState} />}
+
+      />
     </div>
   );
 }
